dece6ca1082dcef3b6d92fb40f18f613681f7c7b
Tom Pearson <tom.g.pearson@gmail.com>
2018-10-13 20:40:08 +0100

    add svg matrix compoinent

--- components/Matrix.js
+++ components/Matrix.js
@@ -1,14 +1,45 @@
 const ItemList = (list)=>{
   return list.map((item,i)=>(
-    <li key={`itemList${i}`}>{item}</li>
+    <g key={`itemList${i}`}  transform={`translate(${item.position.x}, ${item.position.y})`}>
+      <text>{item.label}</text>
+      <circle r={5}></circle>
+    </g>
   ));
 }
 
-const Matrix = ({items=[], title, axes, date, author})=>{
-  return (<div>
-    <h1>{title}</h1>
-    {ItemList(items)}
-  </div>);
+const Axes= ({children, x, y})=>{
+  return (<g>
+    <text>{x[0]} - {x[1]}</text>
+    <text>{y[0]} - {y[1]}</text>
+    <g>{children}</g>
+  </g>);
+}
+
+const Matrix = ({
+  items=[{label:'a', position:{x:1,y:1}}], 
+  title="Title", 
+  axes={
+    x:['xmin','xmax'], 
+    y:['ymin','ymax']}, 
+  date=new Date(), 
+  author='author'
+})=>{
+  const height = 500;
+  const width = 500;
+  const scaledItems = items.map((d,i)=>{
+    return {
+      label:d.label,
+      position:{
+        x:(d.position.x + 1) * (width/2),
+        y:(d.position.y + 1) * (height/2)
+      }
+    }
+  });
+  return (<svg width={width} height={height} viewBox={`0, 0, ${width}, ${height}`}>
+      <Axes x={axes.x} y={axes.y}>
+        {ItemList(scaledItems)}
+      </Axes>
+    </svg>);
 };
 
 export default Matrix;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Link from 'next/link';
 import Matrix from '../components/Matrix';
+
+const exampleData = require('../data/example1.json');
+
 const Index = () => (
   <div>
-    <p>Hello Next.js</p>
+    <h1>{exampleData.title}</h1>
+    <Matrix items={exampleData.items} />
     <Link href="/about"><a>About</a></Link>
-    <Matrix title="My first matrix" items={['a','b','c']} />
   </div>
 );
 
