9bf8b8f697873c36786e2ef02679616daf2121e6
Tom Pearson <tom.g.pearson@gmail.com>
2019-03-11 11:25:34 +0000

    Revert "a commit i wrote about 4 months ago"
    This reverts commit 65fff169e018b3f15b64ed19497bec2c40cbb00b.

--- components/Matrix.js
+++ components/Matrix.js
@@ -1,48 +1,45 @@
-import Axes from './Axes';
-
-function mouseDown(event) {
-  console.log('down', event.clientX, event.clientY);
-}
-
-function mouseUp(event) {
-  console.log('up', event.clientX, event.clientY);
-}
-
 const ItemList = (list)=>{
   return list.map((item,i)=>(
-    <g
-      onMouseDown={mouseDown} 
-      onMouseUp={mouseUp}
-      key={`itemList${i}`}  
-      transform={`translate(${item.position.x}, ${item.position.y})`}
-      draggable="true">
-      <text dy={12}>{item.label}</text>
-      <rect x="0" y="0" width={70} height={12} fill="rgba(0,0,0,0)" stroke="#000" strokeWidth="1"></rect>
+    <g key={`itemList${i}`}  transform={`translate(${item.position.x}, ${item.position.y})`}>
+      <text>{item.label}</text>
       <circle r={5}></circle>
     </g>
   ));
 }
 
-class Matrix extends React.Component {
-  render() {
-    const{ items, axes } = this.props;
-    const height = 500;
-    const width = 500;
-    const scaledItems = items.map((d)=>{
-      return {
-        label:d.label,
-        position:{
-          x:(d.position.x + 1) * (width/2),
-          y:(d.position.y + 1) * (height/2)
-        }
-      }
-    });
-    return (<svg width={width} height={height} viewBox={`0, 0, ${width}, ${height}`}>
-        <Axes x={axes.x} y={axes.y}>
-          {ItemList(scaledItems)}
-        </Axes>
-      </svg>);
-  }
+const Axes= ({children, x, y})=>{
+  return (<g>
+    <text>{x[0]} - {x[1]}</text>
+    <text>{y[0]} - {y[1]}</text>
+    <g>{children}</g>
+  </g>);
 }
+
+const Matrix = ({
+  items=[{label:'a', position:{x:1,y:1}}], 
+  title="Title", 
+  axes={
+    x:['xmin', 'xmax'],
+    y:['ymin', 'ymax']},
+  date=new Date(),
+  author='author'
+})=>{
+  const height = 500;
+  const width = 500;
+  const scaledItems = items.map((d)=>{
+    return {
+      label:d.label,
+      position:{
+        x:(d.position.x + 1) * (width/2),
+        y:(d.position.y + 1) * (height/2)
+      }
+    }
+  });
+  return (<svg width={width} height={height} viewBox={`0, 0, ${width}, ${height}`}>
+      <Axes x={axes.x} y={axes.y}>
+        {ItemList(scaledItems)}
+      </Axes>
+    </svg>);
+};
 
 export default Matrix;
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,14 @@
 import Link from 'next/link';
-import { Provider } from 'react-redux';
 import Matrix from '../components/Matrix';
 
-import {initializeStore} from '../model/store';
 const exampleData = require('../data/example1.json');
 
 const Index = () => (
-  <Provider store={reduxStore}>
-    <div>
-      <h1>{exampleData.title}</h1>
-      <Matrix items={exampleData.items} axes={exampleData.axes} />
-      <Link href="/about"><a>About</a></Link>
-    </div>
-  </Provider>
+  <div>
+    <h1>{exampleData.title}</h1>
+    <Matrix items={exampleData.items} />
+    <Link href="/about"><a>About</a></Link>
+  </div>
 );
 
 export default Index;
